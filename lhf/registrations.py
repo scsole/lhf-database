7a669f72fb884dbf24b1d400488491b6bf07624d
Sebastian Sole <scsole@protonmail.com>
2020-02-20 00:19:16 +1300

    Added registrations structure

---
+++ lhf/registrations.py
@@ -0,0 +1,21 @@
+"""Registrations module for the LHF database."""
+
+def create_db():
+    """Create registrations database."""
+    pass
+
+def add_registrations():
+    """Add new registrations to the database.
+    
+    Registrations are considered new if they have a unique combination
+    of First name, Last name, and DoB.
+    """
+    pass
+
+def create_start_list():
+    """Generate start list for Webscorer."""
+    pass
+
+def create_registrations_list(sort="lname"):
+    """Generate registration list for printing."""
+    pass
