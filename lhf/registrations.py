fb1a61bcd9aea16588a0c9cb7f530969a17f7faf
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-02-20 01:55:11 +1300

    Switched to context managers

--- lhf/registrations.py
+++ lhf/registrations.py
@@ -9,21 +9,21 @@ c = conn.cursor()
 def create_db():
     """Create registrations database."""
     # Dates are stores in TEXT as ISO8601 strings
-    c.execute("""CREATE TABLE IF NOT EXISTS registrations (
-                registration_id INTEGER,
-                first_name TEXT,
-                last_name TEXT,
-                gender TEXT,
-                dob TEXT,
-                club TEXT,
-                email TEXT,
-                medical_conditions TEXT,
-                emergency_name TEXT,
-                emergency_contact TEXT,
-                registration_date TEXT,
-                PRIMARY KEY(registration_id ASC)
-                )""")
-    conn.commit()
+    with conn:
+        c.execute("""CREATE TABLE IF NOT EXISTS registrations (
+                    registration_id INTEGER,
+                    first_name TEXT,
+                    last_name TEXT,
+                    gender TEXT,
+                    dob TEXT,
+                    club TEXT,
+                    email TEXT,
+                    medical_conditions TEXT,
+                    emergency_name TEXT,
+                    emergency_contact TEXT,
+                    registration_date TEXT,
+                    PRIMARY KEY(registration_id ASC)
+                    )""")
 
 def add_registrations():
     """Add new registrations to the database.
@@ -32,12 +32,14 @@ def add_registrations():
     of First name, Last name, DoB and have a newer time stamp than the
     oldest registration.
     """
-    c.execute("INSERT INTO registrations VALUES (1, 'John', 'Doe', 'male', '1/2/1990', 'Leith', 'john.doe@gmail.com', '', 'Jane', '0221112222', '24/11/19')")
-    conn.commit()
+    with conn:
+        c.execute("INSERT INTO registrations VALUES (1, 'John', 'Doe', 'male', '1/2/1990', 'Leith', 'john.doe@gmail.com', '', 'Jane', '0221112222', '24/11/19')")
 
 def create_start_list():
     """Generate start list for Webscorer."""
-    pass
+    c.execute("SELECT registration_id, first_name, last_name FROM registrations")
+    for reg in c.fetchall():
+        print(reg)
 
 def create_registrations_list(sort="lname"):
     """Generate registration list for printing."""
@@ -46,8 +48,5 @@ def create_registrations_list(sort="lname"):
 if __name__ == "__main__":
     create_db()
     add_registrations()
-    c.execute("SELECT * FROM registrations")
-    for reg in c.fetchall():
-        print(reg)
+    create_start_list()
     conn.close()
-    
