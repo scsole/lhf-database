82b90fb96a3b3f991726c4b7c0a739335c93bcb5
Sebastian Sole <scsole@protonmail.com>
2020-02-21 22:22:14 +1300

    Added insert registrations from csv

--- lhf/registrations.py
+++ lhf/registrations.py
@@ -3,15 +3,11 @@
 import csv
 import sqlite3
 
-#conn = sqlite3.connect('lhf.db')
-conn = sqlite3.connect(':memory:')
-c = conn.cursor()
-
 def create_db():
     """Create registrations database."""
     with conn:
         c.execute("""CREATE TABLE IF NOT EXISTS registrations (
-                    registration_id INTEGER,
+                    registration_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     first_name TEXT NOT NULL,
                     last_name TEXT NOT NULL,
                     gender TEXT NOT NULL,
@@ -21,23 +17,57 @@ def create_db():
                     medical_conditions TEXT,
                     emergency_name TEXT,
                     emergency_contact TEXT,
-                    registration_datetime TEXT NOT NULL,
-                    PRIMARY KEY(registration_id ASC)
+                    registration_datetime TEXT NOT NULL
                     )""")
 
-def add_registrations():
+def get_new_registrations(input_file='./input.csv'):
+    """Read registration input (csv) file, return list with new entries.
+
+    We assume that the csv input file only contains valid input.
+    TODO: Skip existing entries found in database
+    """
+    input_registrations = []
+    with open(input_file) as regfile:
+        reader = csv.reader(regfile)
+        next(reader)    # skip headers
+        for row in reader:
+            input_registrations.append(row)
+    return input_registrations
+
+def add_new_registrations():
     """Add new registrations to the database.
     
     Registrations are considered new if they have a unique combination
     of First name, Last name, DoB and have a newer time stamp than the
     oldest registration.
     """
-    sql_insert_statement = "INSERT INTO registrations VALUES (?, ?, ?, ?, date(?), ?, ?, ?, ?, ?, datetime(?))"
+    reglist = get_new_registrations()
     with conn:
-        c.execute(sql_insert_statement, (1, 'John', 'Doe', 'male', '1990-02-01', 'Leith', 'john.doe@gmail.com', '', 'Jane', '0221112222', '1999-11-20 12:01:01'))
-        c.execute(sql_insert_statement, (2, 'Jane', 'Doe', 'male', '1991-02-01', 'Leith', 'jane.doe@gmail.com', '', 'John', '0221111111', '2019-11-20 12:00:00'))
-        c.execute(sql_insert_statement, (3, 'Mike', 'Doe', 'female', '1990-02-02', 'Leith', 'mike.doe@gmail.com', '', 'Jane', '0221112222', '2019-11-20 12:00:01'))
-
+        sql_insert_statement = """INSERT INTO registrations(
+                                first_name,
+                                last_name,
+                                gender,
+                                dob,
+                                club,
+                                email,
+                                medical_conditions,
+                                emergency_name,
+                                emergency_contact,
+                                registration_datetime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+        for reg in reglist:
+            # Only store necessary info in the database
+            reginfo = (reg[2],  # first_name
+                    reg[3],     # last_name
+                    reg[4],     # gender
+                    reg[5],     # dob
+                    reg[7],     # club
+                    reg[1],     # email
+                    reg[8],     # medical_conditions
+                    reg[9],     # emergency_name
+                    reg[10],    # emergency_contact
+                    reg[0],     # registration_datetime
+                    )
+            c.execute(sql_insert_statement, reginfo)
 
 def create_start_list():
     """Generate start list for Webscorer."""
@@ -50,19 +80,19 @@ def create_registrations_list(sort="lname"):
     pass
 
 def last_entry_datetime():
-    """Return the time stamp of the last database registration"""
+    """Return the time stamp of the newest registration"""
     c.execute("SELECT MAX(datetime(registration_datetime)) FROM registrations")
-    print(c.fetchall())
+    return c.fetchone()
 
 if __name__ == "__main__":
+    #conn = sqlite3.connect('lhf.db')
+    conn = sqlite3.connect(':memory:')  # test the database in memory
+    c = conn.cursor()
+
     create_db()
-    add_registrations()
+    add_new_registrations()
     create_start_list()
 
-    c.execute("SELECT * FROM registrations")
-    for reg in c.fetchall():
-        print(reg)
-
-    last_entry_datetime()
+    #print(last_entry_datetime()[0])
 
     conn.close()
