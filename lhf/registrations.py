d88af978f332da9ec24250548db63eea043ae764
Sebastian Sole <sebastian.c.sole@gmail.com>
2020-02-22 15:11:02 +1300

    Added basic duplicate checking

--- lhf/registrations.py
+++ lhf/registrations.py
@@ -8,6 +8,7 @@ import sqlite3
 def create_db():
     """Create registrations database."""
     with conn:
+        # We cannot reuse registration_ids so we must use AUTOINCREMENT.
         c.execute("""CREATE TABLE IF NOT EXISTS registrations (
                     registration_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     first_name TEXT NOT NULL,
@@ -23,29 +24,43 @@ def create_db():
                     )""")
 
 
-def get_new_registrations(input_file='./new_registrations.csv'):
-    """Read registration input (csv) file, return list with new entries.
+def get_new_registrations(reg_input='./new_registrations.csv', dup_output='./duplicate_registrations.csv'):
+    """Read new registration csv file and return a list with new entries.
 
-    We assume that the csv input file only contains valid input.
-    TODO: Skip existing entries found in database
+    We assume that the csv input file already has its contents checked
+    for validity. Duplicate enties are loged to dup_output if any
+    exist. Registrations are considered duplicate if they do not have a
+    unique combination of First name, Last name, and DoB.
     """
-    input_registrations = []
-    with open(input_file) as regfile:
+    newregs = []    # contains all new registrations
+    dupregs = []    # contains all duplicate registrations w/ headers
+    sql_search_statement = """SELECT *
+                            FROM registrations
+                            WHERE first_name = ? COLLATE NOCASE AND last_name = ? COLLATE NOCASE AND dob = ?"""
+
+    with open(reg_input) as regfile:
         reader = csv.reader(regfile)
-        next(reader)    # skip headers
+        dupregs.append(['Existing Registration ID'] + next(reader)) # file headers
         for row in reader:
-            input_registrations.append(row)
-    return input_registrations
+            c.execute(sql_search_statement, (row[2].strip(), row[3].strip(), row[5]))
+            search = c.fetchone()
+            if search is None:
+                newregs.append(row)
+            else:
+                dupregs.append([search[0]] + row)
+
+    # Log skiped duplicate registrations
+    if len(dupregs) > 1:
+        print("Found {} duplicate registrations. They can be found in {}".format(len(dupregs) - 1, dup_output))
+        with open(dup_output, 'w') as dupfile:
+            writer = csv.writer(dupfile)
+            writer.writerows(dupregs)
+
+    return newregs
 
 
-def add_new_registrations():
-    """Add new registrations to the database.
-    
-    Registrations are considered new if they have a unique combination
-    of First name, Last name, DoB and have a newer time stamp than the
-    oldest registration.
-    """
-    reglist = get_new_registrations()
+def add_registrations(reglist):
+    """Add registrations to the database."""
     with conn:
         sql_insert_statement = """INSERT INTO registrations(
                                 first_name,
@@ -60,23 +75,24 @@ def add_new_registrations():
                                 registration_datetime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
         for reg in reglist:
             # Only store necessary info in the database
-            reginfo = (reg[2].strip(),  # first_name
+            reginfo = (
+                    reg[2].strip(),     # first_name
                     reg[3].strip(),     # last_name
                     reg[4].strip(),     # gender
-                    reg[5].strip(),     # dob
+                    reg[5],             # dob
                     reg[7].strip(),     # club
                     reg[1].strip(),     # email
                     reg[8].strip(),     # medical_conditions
                     reg[9].strip(),     # emergency_name
                     reg[10].strip(),    # emergency_contact
-                    reg[0].strip(),     # registration_datetime
+                    reg[0]              # registration_datetime
                     )
             c.execute(sql_insert_statement, reginfo)
 
 
 def create_start_list(race_date=datetime.date.today()):
     """Generate start list for Webscorer."""
-    print(race_date)
+    return race_date
 
 
 def create_registrations_list(outfile='reg_print.csv'):
@@ -105,7 +121,7 @@ if __name__ == "__main__":
     c = conn.cursor()
 
     create_db()
-    add_new_registrations()
+    add_registrations(get_new_registrations())
     create_start_list()
     create_registrations_list()
 
